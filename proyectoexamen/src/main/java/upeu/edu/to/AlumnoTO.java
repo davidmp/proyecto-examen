319ac8af7b794092c504f6c14c48e38db8ac7f20
PC <PC@PCPACIFI>
2012-09-17 23:43:06 -0500

    cambiando nombres

---
+++ proyectoexamen/src/main/java/upeu/edu/to/AlumnoTO.java
@@ -0,0 +1,90 @@
+
+package upeu.edu.to;
+
+
+public class AlumnoTO {
+
+    int id_alumno;
+    String codigo;
+    String nombre;
+    String apellidos;
+    String eap;
+    String sexo;
+    String email;
+    String telefono;
+    String estado;
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getEap() {
+        return eap;
+    }
+
+    public void setEap(String eap) {
+        this.eap = eap;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public int getId_alumno() {
+        return id_alumno;
+    }
+
+    public void setId_alumno(int id_alumno) {
+        this.id_alumno = id_alumno;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    
+}
