ffe12e5126a4dab5b9edf666ee563582a245599a
PC <PC@PCPACIFI>
2012-09-17 20:52:09 -0500

    subiendo el DBConn error en el driver

---
+++ proyectoexamen/src/main/java/upeu/edu/util/DBConn.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package upeu.edu.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author PC
+ */
+public class DBConn {
+   public Connection con=null; 
+public PreparedStatement ps=null;
+public ResultSet rs=null;
+public int i=0;
+
+
+public void getConexionDb(){
+    try {
+        DriverManager.registerDriver(new Driver());
+        if(con==null){
+        con=DriverManager.getConnection("jdbc:postgresql://localhost:5432/cultura","cultura", "123456");
+        
+        System.out.println("Conexion Exitosa.....!!!");
+        }
+    } catch (Exception e) {
+        System.out.println("Error de Conexion "+e.getMessage());
+    }
+
+}
+
+public void getCerrarConexion(){
+    try {
+        if(rs!=null){rs.close();}
+        if(ps!=null){ps.close();}
+        if (con!=null) {con.close();}
+    } catch (Exception e) {
+    }
+} 
+}
